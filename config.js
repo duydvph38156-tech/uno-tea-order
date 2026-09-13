3765f7cedf551b3d0ca511803eb4a1fff64c8d03
duydvph38156-tech <duydvph38156@fpt.edu.vn>
2026-09-14 00:39:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // 3) Paste your Project URL and anon/public key below.
 // Never put a service_role key in this file.
 window.UNO_CONFIG = {
-  SUPABASE_URL: "https://jcebxaijpqszniisdrbf.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://jcebxaijpqszniisdrbf.supabase.",
   SUPABASE_ANON_KEY: "sb_publishable_T7rOCtHrXLDYF-Z5Sdzmyw_9d3UAvnp",
   SHOP_NAME: "UNO TEA",
   SHOP_TAGLINE: "Order tại bàn • Est. 2021",
